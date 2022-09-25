55813d07b2123d59a6067139b33e2c6e864be636
V. Ramachandra Reddy <113619094+vrchreddy@users.noreply.github.com>
2022-09-25 20:59:07 +0530

    Create Cost.py
    #This code decides the amount of admission cost to be charged based on the age of the applicant.

---
+++ Cost.py
@@ -0,0 +1,11 @@
+#This code decides the amount of admission cost to be charged based on the age of the applicant.
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+elif age < 65:
+    price = 40
+else:
+    price = 20
+print(f"Your admission cost is ${price}.")

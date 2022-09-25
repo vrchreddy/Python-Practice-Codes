110143d96bfd9906b85c7ba6d2a0389a27b7f8af
V. Ramachandra Reddy <113619094+vrchreddy@users.noreply.github.com>
2022-09-25 20:27:15 +0530

    Create Name.py
    #This code defines three variables namely, first_name, last_name, and full_name (in the form of an f string) and prints the full name.

---
+++ Name.py
@@ -0,0 +1,12 @@
+#This code defines three variables namely, first_name, last_name, and full_name (in the form of an f string) and prints the full name.
+
+first_name = 'Rama'
+last_name = 'Chandra'
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = 'Ramachandra'
+last_name = 'Reddy'
+full_name = f"{first_name} {last_name}"
+print(full_name.lower())
+print(full_name.upper())
